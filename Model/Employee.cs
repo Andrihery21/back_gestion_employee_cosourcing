876a9a8bf7d30c4961c43cc7c7c954e7f53d6b1c
Andrihery <roadsterandry@gmail.com>
2023-03-21 21:02:22 +0300

    Employee model creation

---
+++ Model/Employee.cs
@@ -0,0 +1,28 @@
+﻿namespace cosourcing_gestion_employee_back.Model
+{
+    public class Employee
+    {
+        public int employeeId { get; set; }
+
+        public string nom { get; set; }
+
+        public string prenom { get; set;}
+
+        public string name { get; set; }
+
+        public string email { get; set; }
+
+        public string poste { get; set; }
+
+        public string salaire { get; set; }
+
+        public string dateEmbauche { get; set; }
+
+        public string avantage { get; set; }
+
+
+
+
+
+    }
+}
